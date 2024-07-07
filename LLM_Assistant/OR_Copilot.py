725acd18482582f9ffda02169a945996e1ea12fa
hyyh28 <384505750@qq.com>
2024-07-07 13:18:52 +0800

    Update OR_Copilot.py

--- LLM_Assistant/OR_Copilot.py
+++ LLM_Assistant/OR_Copilot.py
@@ -4,14 +4,6 @@ from simopt.directory import problem_directory, model_introduction_directory, pr
 from simopt.experiment_base import ProblemSolver, ProblemsSolvers, post_normalize, find_missing_experiments, make_full_metaexperiment, plot_progress_curves, plot_solvability_cdfs, plot_area_scatterplots, plot_solvability_profiles, plot_terminal_progress, plot_terminal_scatterplots
 from langchain_core.prompts import PromptTemplate
 
-llm = ChatOpenAI(
-    model='deepseek-chat',
-    openai_api_key=key,
-    openai_api_base=url,
-    max_tokens=1024
-)
-
-
 user_prompt_of_introduction = PromptTemplate.from_template("You choose {problem} based on {model} model, which is {model_introduction}")
 llm_prompt_of_introduction = PromptTemplate.from_template("The user choose {problem} based on {model} model, which is {model_introduction}")
 problem_setting_prompt = PromptTemplate.from_template("The parameters of {problem} is:\n")
@@ -33,7 +25,7 @@ def get_problem_setting(problem_name="Min Deterministic Function + Noise (SUCG)"
     problem_setting_description = problem_setting_prompt.format(problem=problem_name)
     for factor in problem.factors.keys():
         problem_setting_description += str(factor) + ":" + str(problem.factors[factor]) + "\n"
-    print(problem_setting_description)
+    return problem_setting_description
 
 def get_methods_list_and_introduction():
     solvers = solver_introduction_directory.keys()
@@ -52,3 +44,29 @@ def get_methods_parameters(method="ASTRO-DF (SBCN)"):
     solver_parameters_setting = solver_setting_prompt.format(solver=method)
     solver_parameters_setting += str(specifications)
     return solver_parameters_setting
+
+class ORCopilot(object):
+    def __init__(self, llm_key=key, llm_url=url, max_tokens=1024):
+        self.llm = ChatOpenAI(model='deepseek-chat', openai_api_key=key, openai_api_base=url, max_tokens=max_tokens)
+        self.current_problem_name = None
+        self.current_problem_setting = None
+        self.current_problem_introduction = None
+        self.current_solver = None
+        self.current_recommand_solver = None
+
+    def get_problem(self, problem_name="Min Deterministic Function + Noise (SUCG)"):
+        self.current_problem_name = problem_name
+        self.current_problem_introduction = get_problem_introduction(self.current_problem_name)
+        self.current_problem_setting = get_problem_setting(self.current_problem_name)
+
+    def get_solver(self, method="ASTRO-DF (SBCN)"):
+        self.current_solver = method
+
+    def give_recommand_solver(self):
+        pass
+
+    def give_recommand_solver_parameter(self):
+        pass
+
+
+
